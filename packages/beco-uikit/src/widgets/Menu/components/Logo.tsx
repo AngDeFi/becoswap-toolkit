a809f3326f1a61650321a89caaeba59a794e511b
AngDeFi <84949712+AngDeFi@users.noreply.github.com>
2021-06-04 16:32:27 +0700

    Update Logo.tsx

--- packages/beco-uikit/src/widgets/Menu/components/Logo.tsx
+++ packages/beco-uikit/src/widgets/Menu/components/Logo.tsx
@@ -67,11 +67,11 @@ const Logo: React.FC<Props> = ({ isPushed, togglePush, isDark, href }) => {
         )}
       </MenuButton>
       {isAbsoluteUrl ? (
-        <StyledLink as="a" href={href} aria-label="BecoSwap home page">
+        <StyledLink as="a" href={href} aria-label="PoliexSwap home page">
           {innerLogo}
         </StyledLink>
       ) : (
-        <StyledLink to={href} aria-label="BecoSwap home page">
+        <StyledLink to={href} aria-label="PoliexSwap home page">
           {innerLogo}
         </StyledLink>
       )}
